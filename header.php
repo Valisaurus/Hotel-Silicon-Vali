390ce9936fc72906458ce7c2c17511a0e4c8d9b0
Valisaurus <valley.alosachi@hotmail.com>
2023-01-11 14:50:23 +0100

    meta tag

--- header.php
+++ header.php
@@ -13,6 +13,7 @@ require(__DIR__ . '/calendar.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="an irl web hotel. Book rooms and have fun" />
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/calendar.css">
     <title>Document</title>
